b45cd537985a476813b86eebea6043bfa3980a01
qazruz228 <gi300953@gmail.com>
2026-01-31 05:46:51 +0300

    обновен ScheduleRepository

--- headteacher-service/src/main/java/com/example/headteacherservice/repository/ScheduleRepository.java
+++ headteacher-service/src/main/java/com/example/headteacherservice/repository/ScheduleRepository.java
@@ -5,9 +5,11 @@ import com.example.headteacherservice.entity.enumType.Weekday;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     @Query("""
