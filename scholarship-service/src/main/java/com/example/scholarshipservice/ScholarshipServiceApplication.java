2467b57035e1a408c97f80083bc7eb1a290eb1d9
qazruz228 <gi300953@gmail.com>
2026-02-19 06:56:19 +0300

    Создан новый микросервис scholarship-service

---
+++ scholarship-service/src/main/java/com/example/scholarshipservice/ScholarshipServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.scholarshipservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarshipServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScholarshipServiceApplication.class, args);
+    }
+
+}
